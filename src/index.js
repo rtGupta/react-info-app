d5df328c30172b7b109448e70304c875e5570906
Aarti Gupta <rtgupta@Aartis-MacBook-Air.local>
2024-01-19 18:03:22 -0700

    Added functional components for different sections of the page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,68 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css"
 
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
-)
+/**
+Challenge: 
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(navbar)
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+
+ */
+
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img className="logo" src = "logo192.png" alt="React Logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Fun Facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Gupta development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Page />);
