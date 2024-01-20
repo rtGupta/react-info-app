6702e39ad66af408d20f0420de52d35dab9e8a15
Aarti Gupta <rtgupta@Aartis-MacBook-Air.local>
2024-01-19 20:06:27 -0700

    Created the main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,15 @@ import React from "react";
 
 export function Main() {
     return (
-        <h1>Main content goes here</h1>
+        <main>
+            <h1 className="main-title">Fun Facts about React</h1>
+            <ul className="main-facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export function Navbar() {
     return (
        <nav>
-            <img className="nav-logo" src="favicon.ico" alt="React Logo"></img>
+            <img className="nav-logo" src="images/react-icon-small.png" alt="React Logo"></img>
             <h1 className="nav-title">React Facts</h1>
             <h3 className="nav-sub-title">React Course - Project 1</h3>
        </nav>
